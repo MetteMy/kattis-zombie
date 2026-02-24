da08bcbfc16afb87fc342502d1d53d0d4cd7c2b7
My <mmga@itu.dk>
2026-02-24 11:43:44 +0100

    i forgot

--- input_validators/validate.py
+++ input_validators/validate.py
@@ -2,6 +2,11 @@
 import sys
 import re
 line = sys.stdin.readline()
+a, b = line.split()
+
+if a[0] == '0' or b[0] =='0':
+    print(f"regex failing", file=sys.stderr)
+    sys.exit(43) 
 
 if not re.match(r"[0-9]+ [0-9]+", line):
     print(f"regex failing", file=sys.stderr)
